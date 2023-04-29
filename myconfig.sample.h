fc11a833af02792a3181611b34be8d88b91cb6bc
lucao <lucasromero.cba@hotmail.com>
2023-04-29 01:56:53 -0400

    versao inicial com servidor web streamando camera e reconhecimento de face

---
+++ myconfig.sample.h
@@ -0,0 +1,5 @@
+#define CAM_NAME "ESP32 camera server"
+#define MDNS_NAME "esp32-cam"
+struct station stationList[] = {{"my_ssid","my_password", true}};
+#define CAMERA_MODEL_AI_THINKER       // default
+
